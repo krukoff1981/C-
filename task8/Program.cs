2e5a8b57d292bc709ee49913bcc8d9106550d9f5
krukoff1981 <krukoff1981@yandex.ru>
2022-09-17 23:41:12 +0300

    визуальная доработка №8

--- task8/Program.cs
+++ task8/Program.cs
@@ -1,17 +1,22 @@
 ﻿// вывести все четные числа до введенного числа
-int a = 16;
+Console.WriteLine("Введите число");
+string Number = Console.ReadLine();
+int a = Convert.ToInt32(Number);
 int count = 2;
 int countprog = count;
 if(a < count)
 {
-    Console.WriteLine(0);
+    Console.WriteLine("Здесь нет решений поставленной задачи");
 }
 else
-{
-    while(countprog <= a - count)
     {
-        Console.Write($"{countprog}, ");
-        countprog = countprog + 2;
+    Console.WriteLine("Полученный результат");
+    Console.Write($"{a} -> ");
+    
+            while(countprog <= a - count)
+        {
+            Console.Write($"{countprog}, ");
+            countprog = countprog + 2;
+        }
+        Console.WriteLine(countprog);
     }
-    Console.WriteLine(countprog);
-}
