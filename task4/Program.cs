f0c9d5e3427af1a3ea8d260f97bb994328d0fdef
krukoff1981 <krukoff1981@yandex.ru>
2022-09-17 22:13:41 +0300

    Готовое задание №4

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// из трех введенных чисел выдать максимальное
+Console.WriteLine("Введите первое число");
+string First = Console.ReadLine();
+Console.WriteLine("Введите второе число");
+string Second = Console.ReadLine();
+Console.WriteLine("Введите третье число");
+string Third = Console.ReadLine();
+int a = Convert.ToInt32(First);
+int b = Convert.ToInt32(Second);
+int c = Convert.ToInt32(Third);
+int max = a;
+if(b > max)
+{
+    max = b;
+}
+if(c > max)
+{
+    max = c;
+}
+Console.WriteLine($"{a} {b} {c} -> {max}");
