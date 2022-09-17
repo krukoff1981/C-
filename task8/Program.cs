90e04f28101f8d5e6ea80013810ea940ae8ff216
krukoff1981 <krukoff1981@yandex.ru>
2022-09-17 23:09:06 +0300

    Готовое задание №8

---
+++ task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// вывести все четные числа до введенного числа
+int a = 16;
+int count = 2;
+int countprog = count;
+if(a < count)
+{
+    Console.WriteLine(0);
+}
+else
+{
+    while(countprog <= a - count)
+    {
+        Console.Write($"{countprog}, ");
+        countprog = countprog + 2;
+    }
+    Console.WriteLine(countprog);
+}
