fcaca7a2dcd8a390010faef57909ede0e8820cf9
krukoff1981 <krukoff1981@yandex.ru>
2022-09-17 22:41:30 +0300

    Готовое задание №6

--- task6/Program.cs
+++ task6/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Определить четность введенного числа
+Console.WriteLine("Введите число");
+string Number = Console.ReadLine();
+int Num = Convert.ToInt32(Number);
+Console.WriteLine("число четное?");
+if(Num == 0)
+{
+    Console.WriteLine("0 не является числом для определения четности");
+}
+else
+{
+    if(Num % 2 == 0)
+    {
+        Console.WriteLine($"{Number} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{Number} -> нет");
+    }
+}
