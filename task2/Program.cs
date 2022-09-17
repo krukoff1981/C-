bcfde1229718004b569d7a15481358254cf04950
krukoff1981 <krukoff1981@yandex.ru>
2022-09-17 12:36:56 +0300

    создан ввод данных с терминала

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// из двух введенных чисел указать какое из них max, а какое min
+Console.WriteLine("введите первое число");
+string First = Console.ReadLine();
+Console.WriteLine("введите второе число");
+string Second = Console.ReadLine();
+
+Console.WriteLine("вы ввели");
+Console.WriteLine(First);
+Console.WriteLine(Second);
