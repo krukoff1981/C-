861ec61a9db805f8e632a02a7730d55baba41322
krukoff1981 <krukoff1981@yandex.ru>
2022-09-17 18:12:19 +0300

    Готовое задание №2

--- task2/Program.cs
+++ task2/Program.cs
@@ -1,9 +1,27 @@
 ﻿// из двух введенных чисел указать какое из них max, а какое min
-Console.WriteLine("введите первое число");
+Console.WriteLine("введите число a");
 string First = Console.ReadLine();
-Console.WriteLine("введите второе число");
+Console.WriteLine("введите число b");
 string Second = Console.ReadLine();
-
-Console.WriteLine("вы ввели");
-Console.WriteLine(First);
-Console.WriteLine(Second);
+int FirstNum = Convert.ToInt32(First);
+int SecondNum = Convert.ToInt32(Second);
+if(FirstNum == SecondNum)
+{
+    Console.WriteLine("вы ввели:");
+    Console.WriteLine($"число a {FirstNum}");
+    Console.WriteLine($"число b {SecondNum}");
+    Console.WriteLine("Числа равны");
+}
+else
+{
+    if(FirstNum > SecondNum)
+    {
+        Console.Write($"a = {First}; b = {Second} -> ");
+        Console.WriteLine($"max = {First}; min = {Second}");
+    }
+    else
+    {
+        Console.Write($"a = {First}; b = {Second} -> ");
+        Console.WriteLine($"max = {Second}; min = {First}");
+    }
+}
