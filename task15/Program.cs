38e3f2df8e618f56d4510674b0dd78394821018d
krukoff1981 <krukoff1981@yandex.ru>
2022-09-20 21:58:08 +0300

    Готовое задание №15 - ветка second

--- task15/Program.cs
+++ task15/Program.cs
@@ -1,3 +1,37 @@
 ﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным
 // Эту задачу решу способом сравнения введенного числа и условных обозначений дней недели
 
+Console.WriteLine("Даны дни недели");
+Console.WriteLine("Понедельник - 1");
+Console.WriteLine("Вторник - 2");
+Console.WriteLine("Среда - 3");
+Console.WriteLine("Четверг - 4");
+Console.WriteLine("Пятница - 5");
+Console.WriteLine("Суббота - 6");
+Console.WriteLine("Воскресенье - 7");
+Console.WriteLine("Введите номер дня недели");
+int number = Convert.ToInt32(Console.ReadLine());
+if(number > 0)
+{
+    if(number<6)
+    {
+        Console.WriteLine("Этот день - выходной?");
+        Console.WriteLine($"{number} -> нет");
+    }
+    else
+    {  
+        if(number<8)
+        {
+            Console.WriteLine("Этот день - выходной?");
+        Console.WriteLine($"{number} -> да");
+        }
+        else
+        {
+            Console.WriteLine("Вы ввели неверное число");
+        }
+    }
+}
+else
+{
+    Console.WriteLine("Вы ввели неверное число");
+}
