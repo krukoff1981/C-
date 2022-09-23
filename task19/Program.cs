75cd24295da10d6ec840733e5d3ef207fc223bfb
krukoff1981 <krukoff1981@yandex.ru>
2022-09-23 21:32:02 +0300

    Готовое задание №19

--- task19/Program.cs
+++ task19/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+switch (number)
+{
+    case < 10000:
+    Console.WriteLine("число не пятизначное");
+    break;
+    case < 100000:
+    int numbermem = number;
+    int a = number % 10;
+    number = number / 10;
+    int b = number % 10;
+    number = number / 10;
+    int c = number % 10;
+    number = number / 10;
+    int d = number % 10;
+    number = number / 10;
+    int e = number % 10;
+    if(a == e)
+    {
+        if(b == d)
+        {
+            Console.WriteLine($"{numbermem} => да");
+        }
+        else
+        {
+            Console.WriteLine($"{numbermem} => нет");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"{numbermem} => нет");
+    }
+    break;
+    default:
+    Console.WriteLine("число не пятизначное");
+    break;
+}
