84479e96610488c9872ec5aa627caa8878940d19
krukoff1981 <krukoff1981@yandex.ru>
2022-09-23 23:36:20 +0300

    Готовое задание №21

---
+++ task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+int [] firstArray = new int[3];
+int [] secondArray = new int[3];
+int summ = 0;
+for (int i = 0; i < firstArray.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} коордитату точки один");
+    firstArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+for (int i = 0; i < secondArray.Length; i++)
+{
+    Console.WriteLine($"Введите {i + 1} коордитату точки два");
+    secondArray[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Расстояние между точками составляет ");
+for (int i = 0; i < firstArray.Length; i++)
+{
+    summ = summ + (firstArray[i] - secondArray[i]) * (firstArray[i] - secondArray[i]);
+}
+double result = Math.Sqrt(summ);
+Console.WriteLine(result);
