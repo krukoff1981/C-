46a0d7c70d836e92466696a11d3fbf745a4d771e
krukoff1981 <krukoff1981@yandex.ru>
2022-09-29 21:34:51 +0300

    Задание 25 через оператор for

--- task25/Program.cs
+++ task25/Program.cs
@@ -1,11 +1,17 @@
 ﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-int i = 0;
+//int i = 0;
 int degree = 1;
-while(i < b)
+//while(i < b)
+//{
+  //  degree = degree * a;
+  // i++;
+//}
+
+for(int i = 0; i < b; i++)
 {
     degree = degree * a;
-   i++;
 }
+
 Console.WriteLine($"{a},{b} -> {degree}");
