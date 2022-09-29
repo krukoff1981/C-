2d4f661c78f73d406be61c46e0a49f2e1d867236
krukoff1981 <krukoff1981@yandex.ru>
2022-09-29 23:43:34 +0300

    Готовое задание 29

---
+++ task29/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран.
+Console.WriteLine("Введите размер массива");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[size];
+int i = 0;
+
+for(i = 0; i < array.Length; i++)
+{
+    Console.Write($"Введите число с индексом {i} \t");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+i = 0;
+while(i < array.Length-1)
+{
+    Console.Write($"{array[i]}, ");
+    i++;
+}
+Console.Write($"{array[i]} -> [");
+
+i = 0;
+while(i < array.Length-1)
+{
+    Console.Write($"{array[i]}, ");
+    i++;
+}
+Console.WriteLine($"{array[i]}]");
