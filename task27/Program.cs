96c94ab4ad19c3f3dde1d68177a46caa1641c7cc
krukoff1981 <krukoff1981@yandex.ru>
2022-09-29 21:56:30 +0300

    Готовое задание 27

---
+++ task27/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите произвольное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int summ = 0;
+for(int a = number; a > 0; a = a / 10)
+{
+    summ = summ + a % 10;
+}
+Console.WriteLine($"{number} -> {summ}");
